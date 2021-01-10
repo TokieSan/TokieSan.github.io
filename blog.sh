083a5e5a73381d409371834040eef779309344a9
TokieSan <elt0khy@protonmail.com>
2021-01-10 04:43:12 +0200

    testing atom

--- blog.sh
+++ blog.sh
@@ -1,7 +1,7 @@
 #!/bin/sh
 title=$(cat ${1} | head -1 | cut -c 3-)
 postDate=$(date +"%a, %b. %Y - %r")
-xmlDate=$(date +"%Y-%M-%dT%T+2:00")
+xmlDate=$(date +"%Y-%m-%dT%T+2:00")
 postHead=$(cat blogposts/head)
 postAbstract=$(cat ${1} | grep --line-number \#\# | grep 2 | cut -c 6-)
 htmlContent=$(md2html ${1})
@@ -10,16 +10,18 @@ echo "<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name=
 echo ${htmlContent} >> "blogposts/${2}.html"
 echo "</br><em style='color: #333333;'>Posted at ${postDate}</em></br></br>">>"blogposts/${2}.html"
 cat "blogposts/tail" >>  "blogposts/${2}.html"
+
 #atom ress
+
 echo "<feed>
 <title>Tokhy's hub</title>
-<link href=\"atom.xml\" rel=\"self\"/>
+<link href=\"https://tokiesan.github.io/atom.xml\" rel=\"self\"/>
 <updated>"${xmlDate}"</updated>
 ""$(sed -n 5,$(echo ${remNums}-1 | bc)p atom.xml)">atom.xml
 echo "<entry>
 <title>"${title}"</title><content type=\"html\">"${htmlContent}"</content>
 <link href=\"blogposts/${2}.html\"/>
-<id>tag:tokhy.com:posts/"${2}".html</id>
+<id>tag:tokiesan.github.io:blogposts/"${2}".html</id>
 <published>"${xmlDate}"</published>
 </entry>
 </feed>">>atom.xml
