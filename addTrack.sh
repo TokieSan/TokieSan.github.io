72ebbf78cfbcb421ba1fc09253e2504b609be6b0
TokieSan <elt0khy@protonmail.com>
2021-01-18 01:26:50 +0200

    added wishlist and track.

---
+++ addTrack.sh
@@ -0,0 +1,13 @@
+#!/bin/sh
+read -p "Title: " title
+read -p "Description: " desc
+postDate=$(date +"%a, %d %b. %Y")
+
+newElem="<div class=\"content\"><h2>${title} | <span style=\"color: gray; font-size: 22px;font-weight: 200;\">${postDate}</span></h2><p>${desc}</p></div>
+"
+awk -v FOO1="${newElem}" '{
+    sub(/<!----->/, FOO1 " <!----->");
+    print;
+}' track.html>tmp
+cat tmp>track.html
+rm -rf tmp
