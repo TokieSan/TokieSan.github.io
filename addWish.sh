7da83784f2bf9a22e53462e04ab2972ce7e5a456
TokieSan <elt0khy@protonmail.com>
2021-01-25 06:24:51 +0200

    made the font in wishlist smaller cuz it was thicc

--- addWish.sh
+++ addWish.sh
@@ -1,9 +1,9 @@
 #!/bin/sh
 read -p "Title: " title
 read -p "Description: " desc
-postDate=$(date +"%a, %d %b. %Y")
+postDate=$(date +"%d %b. %Y")
 
-newElem="<div class=\"content\"><h2>${title} | <span style=\"color: gray; font-size: 22px;font-weight: 200;\">${postDate}</span></h2><p>${desc}</p></div>
+newElem="<div class=\"content\"><h2 style=\"font-size: 16px;\">${title} | <span style=\"color: gray;\">${postDate}</span></h2><p>${desc}</p></div>
 "
 awk -v FOO1="${newElem}" '{
     sub(/<!----->/, FOO1 " <!----->");
