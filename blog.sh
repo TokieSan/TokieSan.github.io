cda461969faf3cc83b99b6db8a6c5e2386a52b6f
Tokhy99 <elt0khy@aucegypt.edu>
2023-03-03 23:08:08 +0200

    Removed rubbish files, made blog.sh more readable

--- blog.sh
+++ blog.sh
@@ -1,54 +1,84 @@
 #!/bin/sh
-title=$(cat ${1} | head -1 | cut -c 3-)
+
+read -p "File name: " file_name
+read -p "URL alias: " url_alias
+read -p "Summary: " summ
+
+# Get title from the first line of the file
+title=$(sed -n '1s/^#\s*//p' "${file_name}")
+
+# Get the post header from the "head" file
+headerContent=$(cat blogposts/head)
+
+# Get the post abstract from the second line of the file
+postTitle=$(cat ${file_name} | grep --line-number \#\# | grep 2 | cut -c 6-)
+
+# Convert markdown content to HTML
+htmlContent=$(md2html "${file_name}")
+
+# Get the current date and time
+#title=$(cat "${file_name}" | head -1 | cut -c 3-)
+
+# Get the current date and time in the RFC 3339 format and regular format
 postDate=$(date +"%a, %d %b. %Y - %r")
 xmlDate=$(date --rfc-3339=seconds | sed 's/ /T/')
-postHead=$(cat blogposts/head)
-postAbstract=$(cat ${1} | grep --line-number \#\# | grep 2 | cut -c 6-)
-htmlContent=$(md2html ${1})
+
 remNums=$(cat atom.xml | wc -l)
-echo "<!DOCTYPE html> <html lang="en"> <head> <meta charset="UTF-8"> <meta name="viewport" content="width=device-width, initial-scale=1.0"> <title>"${title}"</title>"${postHead} > "blogposts/${2}.html"
-echo ${htmlContent} >> "blogposts/${2}.html"
-echo "<br/><em style='color: #333333;'>Posted at ${postDate}</em><br/><br/>">>"blogposts/${2}.html"
-cat "blogposts/tail" >>  "blogposts/${2}.html"
 
-#atom ress
+# Write the HTML file
+{
+    echo "<!DOCTYPE html>"
+    echo "<html lang=\"en\">"
+    echo "<head>"
+    echo "<meta charset=\"UTF-8\">"
+    echo "<meta name=\"viewport\" content=\"width=device-width, initial-scale=1.0\">"
+    echo "<title>${title}</title>"
+    echo "${headerContent}"
+    echo "</head>"
+    echo "<body>"
+    echo "${htmlContent}"
+    echo "<br/><em style='color: #333333;'>Posted at ${postDate}</em><br/><br/>"
+    echo "</body>"
+    echo "</html>"
+    cat "blogposts/tail"
+} > "blogposts/${url_alias}.html"
 
-read -p "Summary: " summ
+# Add RSS entry
+
 echo "<?xml version=\"1.0\" encoding=\"utf-8\"?>
 <feed xmlns=\"http://www.w3.org/2005/Atom\">
 <title>Tokhy's hub</title>
 <link href=\"https://tokiesan.github.io/atom.xml\" rel=\"self\"/>
 <updated>"${xmlDate}"</updated>
 <author>
-	<name>Ahmed Gamal Eltokhy</name>
+<name>Ahmed Gamal Eltokhy</name>
 </author>
 <id>https://tokiesan.github.io</id>
-""$(sed -n 10,$(echo ${remNums}-1 | bc)p atom.xml)">atom.xml
+""$(sed -n 10,$(echo ${remNums}-1 | bc)p atom.xml)" > atom.xml
+
 echo "<entry>
 <title>"${title}"</title><summary>"${summ}"</summary>""
-<link href=\"https://tokiesan.github.io/blogposts/${2}.html\"/>
+<link href=\"https://tokiesan.github.io/blogposts/${url_alias}.html\"/>
 <updated>"${xmlDate}"</updated>
-<id>https://tokiesan.github.io/blogposts/${2}.html</id>
+<id>https://tokiesan.github.io/blogposts/${url_alias}.html</id>
 </entry>
-</feed>">>atom.xml
+</feed>" >> atom.xml
 
-#add to index page
-newElem="
-<tr>
-     <td class=\"title\"><a href=\"${2}.html\">${title}</a></td>
-	 <td><em>$(date +"%a, %d %b. %Y")</em></td>
-</tr>
-"
+
+
+# Add entry to blogposts/index.html
+newElem="<tr>\n<td class=\"title\"><a href=\"${url_alias}.html\">${title}</a></td>\n<td><em>$(date +"%a, %d %b. %Y")</em></td>\n</tr>"
 
 awk -v FOO1="${newElem}" '{
     sub(/<!---->/, "<!----> " FOO1);
     print;
-}' blogposts/index.html>tmp
-cat tmp>blogposts/index.html
-rm -rf tmp
+}' blogposts/index.html > temporary_output
+cat temporary_output > blogposts/index.html
+rm -rf temporary_output 
 
-mv ${1} junk/
-# git commit new post
-git add "blogposts/${2}.html" "atom.xml" "blogposts/index.html"
+mv ${file_name} junk/
+
+# Add new post to repo
+git add "blogposts/${url_alias}.html" "atom.xml" "blogposts/index.html"
 git commit -m "Added ${title} post"
 git push -u origin
